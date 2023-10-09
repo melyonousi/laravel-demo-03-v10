1276fe23516c0dd3a6f4a40edbb279f496170103
elyonousimohamed <melyonousi@redimpactarabia.com>
2023-10-09 16:34:11 +0100

    update

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Website | under Maintenance</title>
+
+</head>
+
+<body class="antialiased">
+    <div>
+        <h1>Under Maintenance</h1>
+        <p>Site Under Maintenance, please subscribe to notify you when we are ready</p>
+    </div>
+</body>
+
+</html>
